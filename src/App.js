c01c05361079836387eb255ec46f12349d0c757a
Shane Biggs <moxdev1@gmail.com>
2020-04-12 12:20:42 -0400

    02-npm, ESLint & Prettier

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ const Pet = ({ name, animal, breed }) => {
     React.createElement("h1", {}, name),
     React.createElement("h2", {}, animal),
     React.createElement("h2", {}, breed),
-  ])
-}
+  ]);
+};
 
 const App = () => {
   return React.createElement(
@@ -14,22 +14,19 @@ const App = () => {
     React.createElement(Pet, {
       name: "Luna",
       animal: "Dog",
-      breed: "Havanese"
+      breed: "Havanese",
     }),
     React.createElement(Pet, {
       name: "Jimmy",
       animal: "Jackass",
-      breed: "Donkey"
+      breed: "Donkey",
     }),
     React.createElement(Pet, {
       name: "Barnaby",
       animal: "Cat",
-      breed: "Cartoon Cat"
+      breed: "Cartoon Cat",
     })
   );
 };
 
-ReactDOM.render(
-  React.createElement(App),
-  document.getElementById("root")
-);
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
